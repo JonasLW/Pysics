7302d473049dfb28a9ef5b9ff6075ab3cbaf42ba
Jonas Willadsen <jonas.l.willadsen@gmail.com>
2022-03-30 17:52:46 +0200

    Normal vectors using a normal map.
    A sobel transform is used to identify the edges of the terrain, and
    assign a normal vector to each coordinate. Mask overlap is used to find
    coordinates where terrain boundary intersects object body. This gives a
    line of intersection points, and the normal vector is calculated to be
    the average of these points' normal vectors. Increase accuracy by
    finding TOI more precisely. Currently also uses old collision detection
    algorithm to calculate depth of impact. Should rewrite this.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import os
 import numpy as np
+from scipy import signal
 import time
 
 # Note to self: Position, velocity, acceleration should be integer vectors.
@@ -18,14 +19,15 @@ import time
 # Handle multiple points of impact
 # Note: Rotation makes FPS drop to 50 for larger objects
 #----ASSETS-------
-TEST_SPRITE = pg.image.load(os.path.join('Assets', 'TestSprite3.png'))
-TERRAIN = pg.image.load(os.path.join('Assets', 'Terrain3.png'))
+SOBEL_TEST = pg.image.load(os.path.join('Assets', 'SobelTest2.png'))
+TEST_SPRITE = pg.image.load(os.path.join('Assets', 'Circle.png'))
+TERRAIN = pg.image.load(os.path.join('Assets', 'Terrain2.png'))
 try:
-    BACKGROUND = pg.image.load(os.path.join('Assets', 'Background3.png'))
+    BACKGROUND = pg.image.load(os.path.join('Assets', 'Background2.png'))
 except:
     BACKGROUND = TERRAIN
 try:
-    FOREGROUND = pg.image.load(os.path.join('Assets', 'Foreground3.png'))
+    FOREGROUND = pg.image.load(os.path.join('Assets', 'Foreground2.png'))
 except:
     FOREGROUND = TERRAIN
 #---------
@@ -62,7 +64,7 @@ class PhysicsSprite(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
         # surfaces and masks
         self.image = image
-        self.boundary_image = get_outline(image)
+        self.boundary_image = get_boundary(image)
         self.rot_image = image
         self.rot_boundary_image = self.boundary_image
         self.mask = pg.mask.from_surface(self.image)
@@ -82,8 +84,8 @@ class PhysicsSprite(pg.sprite.Sprite):
         self.angle = 0
         self.ang_vel = 0
         self.rebound = 0.5
-        self.friction_s = 0.1
-        self.friction_d = 0.0008  # Uncertain what are appropriate ranges for these
+        self.friction_s = 0.8
+        self.friction_d = 0.7  # Uncertain what are appropriate ranges for these
         self.collision = None
         self.body_overlap = None
         self.overlap_surface = None
@@ -106,13 +108,14 @@ class PhysicsSprite(pg.sprite.Sprite):
         self.rot_boundary_mask = pg.mask.from_surface(self.rot_boundary_image)
         # Detect and resolve collision.
         normal, POI, depth = self.detect_collision(world)
+        normal, POI = self.detect_collision_map(world) # Maybe do this bit at TOI
         n_tangent = rotate_2v(normal, -90)
         if not (normal[0] == 0 and normal[1] == 0):
             r = POI - self.COM
             r_tangent = rotate_2v(r, -90)
             r_para_sq = r.dot(r) - (r.dot(normal))*(r.dot(normal))  # comp. of r perp to normal
             POI_vel = self.v + r_tangent*self.ang_vel*np.pi/180
-            POI_normal_spd = abs(POI_vel.dot(normal))  # Not sure if abs is necessary. If this is positive, something is wrong
+            POI_normal_spd = -POI_vel.dot(normal)  # Not sure if abs is necessary. If this is positive, something is wrong
             POI_tangent_spd = POI_vel.dot(n_tangent)
             # Find TOI
             t_rwnd = depth/POI_normal_spd
@@ -175,8 +178,10 @@ class PhysicsSprite(pg.sprite.Sprite):
             overlap_center = np.array(self.body_overlap.centroid())
             boundary_1_center = np.array(boundary_overlap_1.centroid())
             boundary_2_center = np.array(boundary_overlap_2.centroid())
+            """
             if boundary_1_center[0] == boundary_2_center[0] and boundary_1_center[1] == boundary_2_center[1]:
                 print("dang")
+            """
             displacement = boundary_2_center - boundary_1_center
             normal = normalize_2v(displacement)
             depth = abs_2v(displacement)
@@ -191,6 +196,37 @@ class PhysicsSprite(pg.sprite.Sprite):
         #print(f"Collision detection time: {end-start:.2f} ms")
         return normal, POI, depth
 
+    def detect_collision_map(self, world):
+        # Pretty hacky atm
+        normal = np.array([0,0], dtype=float)
+        POI = np.array([0,0], dtype=float)
+        intersect = self.rot_boundary_mask.overlap(world.boundary_mask, (-1)*self.s)
+        if intersect:
+            # Experimental -------
+            intersect_map = self.rot_mask.overlap_mask(world.boundary_mask, (-1)*self.s) # Or rot_boundary_mask
+            bw_image = intersect_map.to_surface(setcolor=(255,255,255,255), unsetcolor=(0,0,0,0))
+            intersect_array = (pg.surfarray.array2d(bw_image) != 0)
+            x_1, x_2 = self.s[0], self.s[0] + self.rect.width
+            y_1, y_2 = self.s[1], self.s[1] + self.rect.height
+            x_1 = max(x_1, 0)
+            x_2 = min(x_2, world.rect.width)
+            y_1 = max(y_1, 0)
+            y_2 = min(y_2, world.rect.height)
+            a_1 = -min(self.s[0], 0)
+            a_2 = min(self.rect.width, world.rect.width - self.s[0])
+            b_1 = -min(self.s[1], 0)
+            b_2 = min(self.rect.height, world.rect.height - self.s[1])
+            normal_array = np.zeros((self.rect.width, self.rect.height, 2), dtype=float)
+            normal_array[a_1:a_2, b_1:b_2] = world.normal_map[x_1:x_2, y_1:y_2]
+            normal[0] = np.sum(normal_array[:,:,0]*intersect_array)
+            normal[1] = np.sum(normal_array[:,:,1]*intersect_array)
+            normal = normalize_2v(normal)
+            #----------
+            POI = self.s + np.array(intersect_map.centroid())
+            #POI = self.s + np.array(intersect)
+            #normal = world.normal_map[POI[0], POI[1]]
+        return normal, POI
+
 
 class Terrain(pg.sprite.Sprite):
     def __init__(self, background, terrain, foreground):
@@ -198,9 +234,10 @@ class Terrain(pg.sprite.Sprite):
         self.background = background
         self.terrain = terrain
         self.foreground = foreground
-        self.boundary = get_outline(terrain)
+        self.boundary = get_boundary(terrain)
         self.boundary_mask = pg.mask.from_surface(self.boundary)
         self.mask = pg.mask.from_surface(self.terrain)
+        self.normal_map = get_normal_map(self.terrain)
         self.rect = self.mask.get_rect(left=0, top=0)
         self.rebound = 1
 
@@ -243,6 +280,32 @@ def get_outline(image):
     return outline
 
 
+def get_normal_map(image):
+    # Consider smoothing the surface with a gaussian blur first
+    print('Getting normal map...')
+    mask = pg.mask.from_surface(image)
+    bw_image = mask.to_surface(setcolor=(255,255,255,255), unsetcolor=(0,0,0,255))
+    img_array = pg.surfarray.array2d(bw_image)
+    normal_array = np.zeros((image.get_width(), image.get_height(), 2), dtype=float)
+    sobel_kernel_x = np.array([[-1, -2, -1],
+                               [0, 0, 0],
+                               [1, 2, 1]])
+    sobel_kernel_y = np.array([[-1, 0, 1],
+                               [-2, 0, 2],
+                               [-1, 0, 1]])
+    grad_x = signal.convolve2d(img_array, sobel_kernel_x, boundary='symm', mode='same')
+    grad_y = signal.convolve2d(img_array, sobel_kernel_y, boundary='symm', mode='same')
+    normal_array = np.transpose(np.array([grad_x, grad_y], dtype=float), (1,2,0))
+    normal_array = normalize_2v_array(normal_array)
+    """
+    for x in range(image.get_width()):
+        for y in range(image.get_height()):
+            normal_array[x, y] = normalize_2v(np.array((grad_x[x,y], grad_y[x,y])))
+    """
+    print('Got normal map')
+    return normal_array
+
+
 def get_inertia(mask, density):
     width, height = mask.get_size()
     COM = np.array(mask.centroid())
@@ -271,10 +334,21 @@ def rotate_2v(vector, angle_deg):
 
 def normalize_2v(vector):
     mod_squared = vector.dot(vector)
-    if mod_squared < 0.0000000001:
+    if mod_squared < 0.00001:
         return np.array([0, 0], dtype=float)
     else:
         return vector/np.sqrt(mod_squared)
+
+
+def normalize_2v_array(array):
+    mod_squared = array[:,:,0]*array[:,:,0] + array[:,:,1]*array[:,:,1]
+    mod_squared[mod_squared < 0.00001] = 1
+    mod = np.sqrt(mod_squared)
+    normalized = array
+    normalized[:,:,0] = normalized[:,:,0]/mod
+    normalized[:,:,1] = normalized[:,:,1]/mod
+    normalized[(normalized > 0.00001)*(normalized < 0.00001)] = 0
+    return normalized
 
 
 def cross_2v(a, b):
@@ -287,8 +361,6 @@ def draw_window(world, player):
     WIN.blit(world.terrain, player.prev_rect, area=player.prev_rect)
     WIN.blit(player.rot_image, player.rect)
     WIN.blit(world.foreground, player.prev_rect, area=player.prev_rect)
-    if player.body_overlap.count() > 0:
-        WIN.blit(player.overlap_surface, player.rect, area=player.rect)
     pg.display.update()
     #end = pg.time.get_ticks()
     #print(f"Draw time: {end-start:.2f} ms")
@@ -308,6 +380,7 @@ def get_input_dir(keys_pressed):
 
 
 def main():
+    get_normal_map(SOBEL_TEST)
     world = Terrain(BACKGROUND, TERRAIN, FOREGROUND)
     player = PhysicsSprite(TEST_SPRITE, 0.01, 400, 100)
 
